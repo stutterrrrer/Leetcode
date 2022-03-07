f05d9879c26675f1e36744051bc176c9bb8736d2
ian <38461291+stutterrrrer@users.noreply.github.com>
2022-03-07 22:45:51 +0800

    working version of ThreeSum_15.java

--- random/src/ThreeSum_15.java
+++ random/src/ThreeSum_15.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 public class ThreeSum_15 {
 	public List<List<Integer>> threeSum(int[] nums) {
-		//todo: solve the bug of duplicate triplets when `0` is in the triplet.
 		int n = nums.length;
 		Arrays.sort(nums);
-		if (nums[0] > 0 || nums[n - 1] < 0) return null;
-		int firstPositive = n;
+		if (n == 0 || nums[0] > 0 || nums[n - 1] < 0) return new LinkedList<>();
+		int firstPositive = n, zeroCount = 0;
 		for (int i = 0; i < n; i++) {
-			if (nums[i] > 0) {
+			if (nums[i] == 0) zeroCount++;
+			else if (nums[i] > 0) {
 				firstPositive = i;
 				break;
 			}
 		}
 
 		Map<Integer, List<int[]>> twoSumMap = new HashMap<>();
-		for (int i = 0; i < firstPositive; i++)
-			buildTwoSumMap(i, firstPositive, nums, twoSumMap);
-		for (int i = firstPositive; i < n; i++)
-			buildTwoSumMap(i, n, nums, twoSumMap);
+		buildTwoSumMap(0, firstPositive, nums, twoSumMap);
+		buildTwoSumMap(firstPositive, n, nums, twoSumMap);
 
 		List<List<Integer>> triplets = new LinkedList<>();
 		for (final int curNum : nums) {
@@ -27,29 +25,39 @@ public class ThreeSum_15 {
 			if (twoSumList == null) continue;
 			for (int[] addendPair : twoSumList) {
 				List<Integer> triplet = new LinkedList<>();
-				triplet.add(curNum); triplet.add(addendPair[0]); triplet.add(addendPair[1]);
+				triplet.add(curNum);
+				triplet.add(addendPair[0]);
+				triplet.add(addendPair[1]);
 				triplets.add(triplet);
 			}
 			twoSumMap.remove(-curNum);
+		}
+		if (zeroCount >= 3) {
+			List<Integer> tripleZero = new LinkedList<>();
+			for (int i = 0; i < 3; i++) tripleZero.add(0);
+			triplets.add(tripleZero);
 		}
 
 		return triplets;
 	}
 
 	private void buildTwoSumMap(int bgnIndex, int endIndex, int[] nums, Map<Integer, List<int[]>> twoSumMap) {
-		int firstAddend = nums[bgnIndex];
-		for (int j = bgnIndex + 1; j < endIndex; j++) {
-			int secondAddend = nums[j];
-			twoSumMap.putIfAbsent(firstAddend + secondAddend, new LinkedList<>());
-			List<int[]> addendPairs = twoSumMap.get(firstAddend + secondAddend);
-			addendPairs.add(new int[]{firstAddend, secondAddend});
+		for (int i = bgnIndex; i < endIndex; i++) {
+			int firstAddend = nums[i];
+			for (int j = i + 1; j < endIndex; j++) {
+				int secondAddend = nums[j];
+				twoSumMap.putIfAbsent(firstAddend + secondAddend, new LinkedList<>());
+				List<int[]> addendPairs = twoSumMap.get(firstAddend + secondAddend);
+				addendPairs.add(new int[]{firstAddend, secondAddend});
 
-			while ( j < endIndex - 1 && nums[j + 1] == nums[j]) j++;
+				while (j < endIndex - 1 && nums[j + 1] == nums[j]) j++;
+			}
+			while (i < endIndex - 1 && nums[i + 1] == nums[i]) i++;
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] nums = {-1, 0, 1, 2, -1, -4, 0, 0, 0, 0, 1};
+		int[] nums = {-1, 0, 1, 2, -1, -4, 0, 0};
 		ThreeSum_15 solver = new ThreeSum_15();
 		for (List<Integer> triplet : solver.threeSum(nums)) {
 			System.out.println(triplet);
