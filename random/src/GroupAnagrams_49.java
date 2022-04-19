94ced257e300c474e8ba4c0a7f637e9f785e1e0e
ian <38461291+stutterrrrer@users.noreply.github.com>
2022-04-19 16:43:15 +0800

    anagrams - sort & count approach

--- random/src/GroupAnagrams_49.java
+++ random/src/GroupAnagrams_49.java
@@ -1,63 +1,44 @@
-import javax.swing.*;
 import java.util.*;
 
 public class GroupAnagrams_49 {
-	public List<List<String>> groupAnagrams(String[] strs) {
-		HashMap<Integer, List<Integer>> charSetToAnagGroupIndex = new HashMap<>();
-		ArrayList<List<String>> anagramGroups = new ArrayList<>();
 
-		for (String str : strs) {
-			int n = str.length(), sum = 0;
-			for (int i = 0; i < n; i++) sum += str.charAt(i);
-			int hashCode = sum * n;
+    public List<List<String>> groupAnagramsSort(String[] strs) {
+        // converge anagrams into the same string: by sorting a string
+        HashMap<String, List<String>> anagrams = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String sortedStr = new String(arr);
+            anagrams.putIfAbsent(sortedStr, new LinkedList<>());
+            anagrams.get(sortedStr).add(str);
+        }
+        return new LinkedList<>(anagrams.values());
+    }
 
-			if (charSetToAnagGroupIndex.containsKey(hashCode)) {
-				List<Integer> groupIndices = charSetToAnagGroupIndex.get(hashCode);
-				boolean belongsInExistingGroup = false;
-				for (Integer groupIndex : groupIndices) {
-					List<String> anagGroup = anagramGroups.get(groupIndex);
-					if (areAnagrams(anagGroup.get(0), str)) {
-						anagGroup.add(str);
-						belongsInExistingGroup = true;
-						break;
-					}
-				}
-				if (!belongsInExistingGroup) {
-					anagramGroups.add(new LinkedList<>(Collections.singleton(str)));
-					groupIndices.add(anagramGroups.size() - 1);
-				}
-			} else {
-				anagramGroups.add(new LinkedList<>(Collections.singleton(str)));
-				charSetToAnagGroupIndex.put(hashCode, new LinkedList<>(Collections.singleton(anagramGroups.size() - 1)));
-			}
-		}
+    public List<List<String>> groupAnagramsCount(String[] strs) {
+        // converge anagrams into the same string: by counting frequencies of 26 letters
+        // then merging the frequencies into 1 string separated with "#" for each letter, use it as hash key.
+        // foregoes the sorting which takes longer
+        HashMap<String, List<String>> anagrams = new HashMap<>();
+        for (String str : strs) {
+            int[] letterFrequencies = new int[26];
+            for (int i = 0; i < str.length(); i++) letterFrequencies[str.charAt(i) - 'a']++;
+            StringBuilder hashKeyBuilder = new StringBuilder();
+            for (int i : letterFrequencies) hashKeyBuilder.append(i + "#");
+            final String key = hashKeyBuilder.toString();
+            anagrams.putIfAbsent(key, new LinkedList<>());
+            anagrams.get(key).add(str);
+        }
+        return new LinkedList<>(anagrams.values());
+    }
 
-		return anagramGroups;
-	}
-
-	private boolean areAnagrams(String s1, String s2) {
-		int n = s1.length();
-		if (s2.length() != n) return false;
-		HashMap<Character, Integer> charCounts = new HashMap<>();
-		for (int i = 0; i < n; i++) {
-			char c = s1.charAt(i);
-			charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
-		}
-		for (int i = 0; i < n; i++) {
-			Integer count = charCounts.get(s2.charAt(i));
-			if (count == null || count == 0) return false;
-			else charCounts.put(s2.charAt(i), count - 1);
-		}
-		return true;
-	}
-
-	public static void main(String[] args) {
-		String[] strs = {"ray", "cod", "abe", "ned", "arc", "jar", "owl", "pop", "paw", "sky", "yup", "fed", "jul",
-				"woo", "ado", "why", "ben", "mys", "den", "dem", "fat", "you", "eon", "sui", "oct", "asp", "ago",
-				"lea", "sow", "hus", "fee", "yup", "eve", "red", "flo", "ids", "tic", "pup", "hag", "ito", "zoo"};
-		String[] strs1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
-		GroupAnagrams_49 solver = new GroupAnagrams_49();
-		System.out.println(solver.groupAnagrams(strs1));
-	}
+    public static void main(String[] args) {
+        String[] strs = {"ray", "cod", "abe", "ned", "arc", "jar", "owl", "pop", "paw", "sky", "yup", "fed", "jul",
+                "woo", "ado", "why", "ben", "mys", "den", "dem", "fat", "you", "eon", "sui", "oct", "asp", "ago",
+                "lea", "sow", "hus", "fee", "yup", "eve", "red", "flo", "ids", "tic", "pup", "hag", "ito", "zoo"};
+        String[] strs1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        GroupAnagrams_49 solver = new GroupAnagrams_49();
+        System.out.println(solver.groupAnagramsSort(strs));
+    }
 }
 
