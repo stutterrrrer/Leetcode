04f7693c7f71284881c6e5085f712de50ca314b9
ian <38461291+stutterrrrer@users.noreply.github.com>
2022-04-27 18:04:18 +0800

    indentation

--- random/src/MinStack_155.java
+++ random/src/MinStack_155.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class MinStack_155 {
+import java.util.Stack;
+
+class MinStack {
+
+    private class valMinPair {
+        int val;
+        int minWhenInserted;
+
+        public valMinPair(int val, int minWhenInserted) {
+            this.val = val;
+            this.minWhenInserted = minWhenInserted;
+        }
+    }
+    private Stack<valMinPair> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        int prevMin = stack.empty()? Integer.MAX_VALUE : stack.peek().minWhenInserted;
+        stack.push(new valMinPair(val, Math.min(prevMin, val)));
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek().val;
+    }
+
+    public int getMin() {
+        return stack.peek().minWhenInserted;
+    }
 }
--- random/src/MinimumRemoveValidParentheses_1249.java
+++ random/src/MinimumRemoveValidParentheses_1249.java
@@ -1,50 +1,59 @@
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
-public class MinimiumRemoveValidParentheses_1249 {
-	public String minRemoveToMakeValid1(String s) {
-		// use a index stack and StringBuilder.removeCharAt() method, which takes O(N) time.
-		Stack<Integer> leftParenIndices = new Stack<>();
-		StringBuilder ans = new StringBuilder();
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if (c == '(') leftParenIndices.add(i);
-			else if (c == ')') {
-				if (leftParenIndices.isEmpty()) continue; // don't pop nor append
-				else leftParenIndices.pop();
-			}
-			ans.append(c);
-		}
-		while (!leftParenIndices.isEmpty()) ans.deleteCharAt(leftParenIndices.pop());
-		return ans.toString();
-	}
+public class MinimumRemoveValidParentheses_1249 {
+    public String minRemoveToMakeValid1(String s) {
+        // use an index stack, 2 passes
+        Stack<Integer> leftParenIndices = new Stack<>();
+        Set<Integer> indicesToRemove = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') leftParenIndices.add(i);
+            else if (c == ')') {
+                if (leftParenIndices.isEmpty()) indicesToRemove.add(i);
+                else leftParenIndices.pop();
+            }
+        }
+        while (!leftParenIndices.isEmpty()) indicesToRemove.add(leftParenIndices.pop());
 
-	public String minRemoveToMakeValid2(String s) {
-		// no stack, 2 passes, all O(1) operations
-		int unmatchedLeftParen = 0, remValidLeftParen = 0;
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if (c == '(') unmatchedLeftParen++;
-			else if (c == ')' && unmatchedLeftParen > 0) {
-				remValidLeftParen++;
-				unmatchedLeftParen--;
-			}
-		}
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+            if (!indicesToRemove.contains(i)) ans.append(s.charAt(i));
+        return ans.toString();
+    }
 
-		StringBuilder ans = new StringBuilder();
-		int remValidRightParen = remValidLeftParen;
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if ((c == '(' && remValidLeftParen-- <= 0) ||
-					(c == ')' && remValidRightParen <= Math.max(remValidLeftParen, 0))) {
-				continue;
-			}
-			ans.append(c);
-		}
-		return ans.toString();
-	}
+    public String minRemoveToMakeValid2(String s) {
+        // no stack, 2 passes
+        int unmatchedLeftParen = 0, remValidLeftParen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') unmatchedLeftParen++;
+            else if (c == ')' && unmatchedLeftParen > 0) {
+                remValidLeftParen++;
+                unmatchedLeftParen--;
+            }
+        }
 
-	public static void main(String[] args) {
-		String input = "lee(t(c)o)de)";
-		
-	}
+        StringBuilder ans = new StringBuilder();
+        int remValidRightParen = remValidLeftParen;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(')
+                if (remValidLeftParen > 0) remValidLeftParen--;
+                else continue;
+            else if (c == ')')
+                if (remValidRightParen > remValidLeftParen) remValidRightParen--;
+                else continue;
+            ans.append(c);
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "))(()abc()";
+        MinimumRemoveValidParentheses_1249 solver = new MinimumRemoveValidParentheses_1249();
+        System.out.println(solver.minRemoveToMakeValid1(input));
+        System.out.println(solver.minRemoveToMakeValid2(input));
+    }
 }
