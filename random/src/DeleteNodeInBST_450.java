a190d3a722ef3d3bd50072bf1637f99d4aa94021
ian <38461291+stutterrrrer@users.noreply.github.com>
2022-05-01 22:16:55 +0800

    Kth smallest (K-ranking) node in the binary search tree - iterative stack implementation of in-order tree traversal

---
+++ random/src/DeleteNodeInBST_450.java
@@ -0,0 +1,43 @@
+public class DeleteNodeInBST_450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        int curVal = root.val;
+        if (key > curVal) root.right = deleteNode(root.right, key);
+        else if (key < curVal) root.left = deleteNode(root.left, key);
+        else // root.val == key:
+            root = replace(root);
+        return root;
+    }
+
+    private TreeNode replace(TreeNode root) {
+        // base case - the deleted node is a leaf node.
+        if (root.left == null && root.right == null) return null;
+
+        // recursive calls: if the deleted node has at least 1 child:
+        if (root.left != null) {
+            root.val = findMax(root.left);
+            root.left = deleteNode(root.left, root.val);
+        } else { // origLeft == null && origRight != null:
+            root.val = findMin(root.right);
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private int findMax(TreeNode root) {
+        while (root.right != null) root = root.right;
+        return root.val;
+    }
+
+    private int findMin(TreeNode root) {
+        while (root.left != null) root = root.left;
+        return root.val;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = ConstructTree.constructTree("[5,3,6,2,4,null,7]");
+        int key = 3;
+        DeleteNodeInBST_450 solver = new DeleteNodeInBST_450();
+
+    }
+}
---
+++ random/src/KthSmallestInBST_230.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class KthSmallestInBST_230 {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> nodesWithBiggerValues = new Stack<>();
+        while (true) {
+            if (root != null) {
+                nodesWithBiggerValues.push(root); // visit this node later
+                root = root.left; // go to the smaller node first
+            } else { // when cur == null -> the top of the stack is the smallest remaining node.
+                if (nodesWithBiggerValues.isEmpty()) break; // all nodes have been visited.
+                // "visit" the smallest of the remaining nodes:
+                root = nodesWithBiggerValues.pop();
+                if (--k == 0) return root.val;
+                root = root.right; // there's no smaller node than root - go to the next node that's bigger than root
+            }
+        }
+        return -1;
+    }
+
+    public void inOrderPrint(TreeNode root) {
+        Stack<TreeNode> nodesWithBiggerValues = new Stack<>();
+        while (true) {
+            if (root != null) {
+                nodesWithBiggerValues.push(root); // visit this node later
+                root = root.left; // go to the smaller node first
+            } else { // when cur == null -> top of the stack is the smallest remaining node
+                if (nodesWithBiggerValues.isEmpty()) break; //all nodes have been visited;
+
+                // "visit" the smallest of the remaining nodes:
+                root = nodesWithBiggerValues.pop();
+                System.out.print(root.val + ", ");
+                // there's no smaller node than root, so go to next node, which is bigger than root
+                root = root.right;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = ConstructTree.constructTree("[3,1,4,null,2]");
+        int rank = 4;
+        KthSmallestInBST_230 solver = new KthSmallestInBST_230();
+        solver.inOrderPrint(root);
+    }
+}
