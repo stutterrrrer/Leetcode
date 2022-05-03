2cead0515e772af46890bf9afb1a84c0da9374b2
ian <38461291+stutterrrrer@users.noreply.github.com>
2022-05-03 11:42:26 +0800

    revamped lowest ancestor post order stack to better mimic recursion process

--- random/src/LowestCommonAncestor_236.java
+++ random/src/LowestCommonAncestor_236.java
@@ -33,73 +33,77 @@ public class LowestCommonAncestor_236 {
 
     public TreeNode lowestCommonAncestorIntuitiveStack(TreeNode root, TreeNode p, TreeNode q) {
         Stack<NodeStatus> stack = new Stack<>();
-        NodeStatus cur = new NodeStatus(root);
-        while (true) {
-            if (cur.node == null) cur = stack.pop(); // base case 1
-
-            if (cur.visitedSubtrees < 2) { // go to left & right subtrees first
-                cur.visitedSubtrees++;
-                stack.push(cur);
-                cur = new NodeStatus(cur.visitedSubtrees == 1 ? cur.node.left : cur.node.right);
-                continue;
+        stack.push(new NodeStatus(root));
+        while (!stack.isEmpty()) {
+            NodeStatus cur = stack.peek();
+            if (cur.node == null) {
+                stack.pop(); // "return" to parent
+            } else if (cur.visitedSubtrees < 2) { // go to left & right subtrees first
+                stack.push(new NodeStatus(cur.visitedSubtrees++ == 0 ? cur.node.left : cur.node.right));
+            } else { // if (cur.visitedSubtrees == 2): add this subtree's #of found descendants to the parent's.
+                if (cur.node.val == p.val || cur.node.val == q.val) cur.descendants++;
+                if (cur.descendants == 2) return cur.node; // return before traversing whole tree; beats recursion
+                stack.pop(); // "return" to parent
+                stack.peek().descendants += cur.descendants; // if cur is ancestor, cur's parent is also an ancestor
             }
-            // if (cur.visitedSubtrees == 2): base case 2 - add this subtree's #of found descendants to the parent's.
-            if (cur.node == p || cur.node == q) cur.descendants++;
-            if (cur.descendants == 2) return cur.node; // return before traversing whole tree; beats recursion
-            stack.peek().descendants += cur.descendants; // if cur is ancestor, cur's parent is also an ancestor
-            cur = stack.pop();
         }
+        return null;
     }
 
     private static class Node3Stages {
         public TreeNode node;
         public int visitedSubTrees;
 
-        public Node3Stages(TreeNode node, int visitedSubTrees) {
+        public Node3Stages(TreeNode node) {
             this.node = node;
-            this.visitedSubTrees = visitedSubTrees;
         }
     }
 
     public TreeNode lowestCommonAncestorCleverStack(TreeNode root, TreeNode p, TreeNode q) {
         int found = 0, pVal = p.val, qVal = q.val;
-        TreeNode lowestAncestorToFirst = root;
+        TreeNode lowestAncestorOnStacToFirstFound = root;
         Stack<Node3Stages> stack = new Stack<>(); // Integer: #of visited subtrees of this node
-        Node3Stages cur = new Node3Stages(root, 0);
-        while (true) {
-            if (cur.node == null) cur = stack.pop();
-            // first time reaching this node (not after coming back up from its children)
+        stack.push(new Node3Stages(root));
+        while (!stack.isEmpty()) {
+            Node3Stages cur = stack.peek();
+            if (cur.node == null) {
+                stack.pop();
+                continue;
+            }
+            // if cur.node != null:
             if (cur.visitedSubTrees == 0 && (cur.node.val == pVal || cur.node.val == qVal))
-                if (++found == 1) lowestAncestorToFirst = cur.node;
+                // first time reaching this node (not after coming back up from its children)
+                if (++found == 1) lowestAncestorOnStacToFirstFound = cur.node;
+
             if (found == 2) { // no need to go further down to subtrees - common ancestor is above
-                while (cur.node != lowestAncestorToFirst) cur = stack.pop();
-                return cur.node;
+                return lowestAncestorOnStacToFirstFound; // because any node still on stack is cur's ancestor
             } else { // if found < 2:
-                if (cur.visitedSubTrees < 2) { // visit subtrees, if it has subtrees.
-                    stack.push(cur);
-                    cur = new Node3Stages(++cur.visitedSubTrees == 1 ? cur.node.left : cur.node.right, 0);
-                    continue;
+                if (cur.visitedSubTrees < 2) { // visit subtrees
+                    stack.push(new Node3Stages(++cur.visitedSubTrees == 1 ? cur.node.left : cur.node.right));
+                } else { // if both of the subtrees of curNode has been visited:
+                    stack.pop(); // "return" to cur's parent - by popping off cur
+                    if (found == 1 && cur.node == lowestAncestorOnStacToFirstFound)
+                        lowestAncestorOnStacToFirstFound = stack.peek().node; // because all nodes on the stack is cur.node's ancestor
                 }
-                // if both of the subtrees of curNode has been visited:
-                if (found == 1 && cur.node == lowestAncestorToFirst)
-                    lowestAncestorToFirst = stack.peek().node;
-                cur = stack.pop();
             }
         }
+        return null;
     }
 
     public void postOrderPrint(TreeNode root) {
         Stack<Node3Stages> stack = new Stack<>();
-        stack.push(new Node3Stages(root, 0));
+        stack.push(new Node3Stages(root));
         while (!stack.isEmpty()) {
-            Node3Stages cur = stack.pop();
-            if (cur.node == null) continue;
-            if (cur.visitedSubTrees < 2) {
-                stack.push(cur);
+            Node3Stages cur = stack.peek(); // peek()! not pop()
+            if (cur.node == null) {
+                stack.pop(); // "return" to parent
+            } else if (cur.visitedSubTrees < 2) { // "recursively call" on the left / right subtree
                 stack.push(new Node3Stages(
-                        cur.visitedSubTrees++ == 0 ? cur.node.left : cur.node.right
-                        , 0));
-            } else System.out.print(cur.node.val + ", ");
+                        cur.visitedSubTrees++ == 0 ? cur.node.left : cur.node.right));
+            } else { // print and "return" to parent
+                System.out.print(cur.node.val + ", ");
+                stack.pop();
+            }
         }
     }
 
@@ -111,9 +115,8 @@ public class LowestCommonAncestor_236 {
 
     public static void main(String[] args) {
         TreeNode root = ConstructTree.constructTree("[3,5,1,6,2,0,8,null,null,7,4]");
-//        int pVal = 5, qVal = 1;
+        int pVal = 5, qVal = 1;
         LowestCommonAncestor_236 solver = new LowestCommonAncestor_236();
-//        System.out.println(solver.testHelper(root, pVal, qVal).val);
-        solver.postOrderPrint(root);
+        System.out.println(solver.testHelper(root, pVal, qVal).val);
     }
 }
