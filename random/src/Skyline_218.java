b74213011ea40f8832506ee4449985c96b966ded
ian <38461291+stutterrrrer@users.noreply.github.com>
2022-08-01 11:59:35 +0800

    added comments and an if check: halved the runtime

--- random/src/Skyline_218.java
+++ random/src/Skyline_218.java
@@ -11,26 +11,27 @@ public class Skyline_218 {
         // at start of each iteration, all heap elements will have i[0] < cur[1]
         PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(i -> -i[2]));
 
-        for (int nextI = 0; nextI < n; ) {
+        for (int nextI = 0; nextI < n; ) { // at each cur building, check the next building with a higher X-coordinate
             int[] next = buildings[nextI];
-            if (next[0] < cur[1]) { // if there's overlap
+            if (next[0] < cur[1]) { // if there's overlap between cur and next
                 nextI = addToHeap(buildings, n, heap, nextI, next);
-                while (heap.peek()[1] <= cur[0]) heap.remove();
-                if (heap.peek()[2] > cur[2]) { // if the tallest overlapping building is taller than cur
-                    heap.add(cur);
-                    cur = heap.remove();
+                while (heap.peek()[1] <= cur[0]) heap.remove(); // remove invalid buildings on heap
+                next = heap.peek();
+                if (next[2] > cur[2]) { // tallest overlapping next is taller than cur: new critical point
+                    if (cur[1] > next[1]) heap.add(cur); // put on heap because it has more horizontal length
+                    cur = heap.remove(); // i.e. cur = next
                     contour.add(List.of(cur[0], cur[2]));
                 }
-            } else {
-                if (next[0] == cur[1]) // touching
+            } else { // if there's no overlap - either cur and next are touching, or there's a gap between them:
+                if (next[0] == cur[1]) // touching: then the next building is also a potential critical point
                     nextI = addToHeap(buildings, n, heap, nextI, next);
-                while (heap.peek()[1] <= cur[1]) heap.remove();
-                next = heap.remove();
+                while (heap.peek()[1] <= cur[1]) heap.remove(); // remove invalid buildings on heap
+                next = heap.remove(); // the tallest building (or floor) at x = cur[1] (cur's right end)
                 if (next[2] != cur[2]) contour.add(List.of(cur[1], next[2]));
                 cur = next;
             }
         } // after all buildings have been examined
-        while (heap.size() > 1) {
+        while (heap.size() > 1) { // the floor will be the last remaining
             int[] next = heap.remove();
             if (next[1] > cur[1]) {
                 if (next[2] != cur[2]) contour.add(List.of(cur[1], next[2]));
